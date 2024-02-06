b1cc1a7f2b3e8531d5157e7efbef3ec7f73964a0
AleixJuR <154283119+AleixJuR@users.noreply.github.com>
2024-02-06 19:39:03 +0100

    Agregar archivos de proyecto.

---
+++ quizGithub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace quizGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
