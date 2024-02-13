a6759643bffbd935642c1ac0b8a60232e1280ba2
Arnau Cabratosa <a2308739@institutmontilivi.cat>
2024-02-13 19:53:26 +0100

    Hola 2

--- quizGithub/Program.cs
+++ quizGithub/Program.cs
@@ -47,9 +47,9 @@
             Console.WriteLine("1 -- Topic1");
             Console.WriteLine("2 -- Topic2");
             Console.WriteLine("3 -- Topic3");
-            Console.WriteLine("4 -- Topic4");
-            Console.WriteLine("5 -- Topic5");
-            Console.WriteLine("6 -- Topic6");
+            Console.WriteLine("4 -- Geography");
+            Console.WriteLine("5 -- Technology");
+            Console.WriteLine("6 -- Famous_Personalities");
             Console.WriteLine("0 -- Exit");
         }
         
@@ -68,23 +68,23 @@
             MsgNextScreen("Press a key to go to the main menu");
         }
 
-        public static void Topic4()
+        public static void Geography()
         {
             ConsoleKeyInfo resposta = default(ConsoleKeyInfo);
-            ConsoleKey[] correctes = { ConsoleKey.A, ConsoleKey.D, ConsoleKey.E, ConsoleKey.B, ConsoleKey.A };
+            ConsoleKey[] correctes = { ConsoleKey.B, ConsoleKey.B, ConsoleKey.A, ConsoleKey.C, ConsoleKey.D }; // These ones are the correct.
             string[] preguntes = {
-                "Pregunta1",
-                "Pregunta2",
-                "Pregunta3",
-                "Pregunta4",
-                "Pregunta5"
+                "What is the capital city of Japan",
+                "Which river is the longest in the world?",
+                "What is the largest desert in the world?",
+                "Which mountain range stretches across the northern border of India?",
+                "What is the largest ocean in the world?"
             };
             string[,] respostes = {
-                { "resposta1", "resposta2", "resposta3", "resposta4" },
-                { "resposta1", "resposta2", "resposta3", "resposta4" },
-                { "resposta1", "resposta2", "resposta3", "resposta4" },
-                { "resposta1", "resposta2", "resposta3", "resposta4" },
-                { "resposta1", "resposta2", "resposta3", "resposta4"},
+                { "A) Beijing", "B) Tokyo", "C) Seoul", "D) Bangkok" },
+                { "A) Amazon river", "B) Nile river", "C) Mississippi river", "D) Yangtze river" },
+                { "A) Sahara desert", "B) Gobi desert", "C) Arabian desert", "D) Kalahati desert" },
+                { "A) Alps", "B) Andes", "C) Himalaya", "D) Rocky mountains" },
+                { "A) Indian Ocean", "B) Atlantic ocean", "C) Southern Ocean", "D) Pacific Ocean"},
             };
             bool validAnswer = false;
             int encerts = 0;
@@ -95,7 +95,7 @@
                 {
                     try
                     {
-                        Console.WriteLine($"Pregunta {i + 1} --> {preguntes[i]}");
+                        Console.WriteLine($"Question {i + 1} --> {preguntes[i]}");
                         for (int y = 0; y < respostes.GetLength(1); y++)
                         {
                             Console.WriteLine(respostes[i, y]);
@@ -121,26 +121,26 @@
                 validAnswer = false;
             }
             Console.WriteLine($"You have finised the test with {encerts} correct answers");
-            MsgNextScreen("Press a key to go to the main menu");
+            MsgNextScreen("Press any key to go to the main menu");
         }
 
-        public static void Topic5()
+        public static void Technology()
         {
             ConsoleKeyInfo resposta = default(ConsoleKeyInfo);
-            ConsoleKey[] correctes = { ConsoleKey.A, ConsoleKey.D, ConsoleKey.E, ConsoleKey.B, ConsoleKey.A };
+            ConsoleKey[] correctes = { ConsoleKey.A, ConsoleKey.A, ConsoleKey.B, ConsoleKey.B, ConsoleKey.C };
             string[] preguntes = {
-                "Pregunta1",
-                "Pregunta2",
-                "Pregunta3",
-                "Pregunta4",
-                "Pregunta5"
+                "Who is the CEO of Tesla, Inc.?",
+                "What does CPU stand for in computing?",
+                "Which company developed the Windows operating system?",
+                "What is the programming language used to develop Android apps?",
+                "Which social media platform was founded by Mark Zuckerberg?"
             };
             string[,] respostes = {
-                { "resposta1", "resposta2", "resposta3", "resposta4" },
-                { "resposta1", "resposta2", "resposta3", "resposta4" },
-                { "resposta1", "resposta2", "resposta3", "resposta4" },
-                { "resposta1", "resposta2", "resposta3", "resposta4" },
-                { "resposta1", "resposta2", "resposta3", "resposta4"},
+                { "A) Beijing", "B) Tokyo", "C) Seoul", "D) Bangkok" },
+                { "A) Beijing", "B) Tokyo", "C) Seoul", "D) Bangkok" },
+                { "A) Beijing", "B) Tokyo", "C) Seoul", "D) Bangkok" },
+                { "A) Beijing", "B) Tokyo", "C) Seoul", "D) Bangkok" },
+                { "A) Beijing", "B) Tokyo", "C) Seoul", "D) Bangkok" },
             };
             bool validAnswer = false;
             int encerts = 0;
@@ -151,7 +151,7 @@
                 {
                     try
                     {
-                        Console.WriteLine($"Pregunta {i + 1} --> {preguntes[i]}");
+                        Console.WriteLine($"Question {i + 1} --> {preguntes[i]}");
                         for (int y = 0; y < respostes.GetLength(1); y++)
                         {
                             Console.WriteLine(respostes[i, y]);
@@ -177,11 +177,11 @@
                 validAnswer = false;
             }
             Console.WriteLine($"You have finised the test with {encerts} correct answers");
-            MsgNextScreen("Press a key to go to the main menu");
+            MsgNextScreen("Press any key to go to the main menu");
 
         }
 
-        public static void Topic6()
+        public static void Famous_Personalities()
         {
             ConsoleKeyInfo resposta = default(ConsoleKeyInfo);
             ConsoleKey[] correctes = { ConsoleKey.A, ConsoleKey.D, ConsoleKey.E, ConsoleKey.B, ConsoleKey.A };
@@ -208,7 +208,7 @@
                 {
                     try
                     {
-                        Console.WriteLine($"Pregunta {i + 1} --> {preguntes[i]}");
+                        Console.WriteLine($"Question {i + 1} --> {preguntes[i]}");
                         for (int y = 0; y < respostes.GetLength(1); y++)
                         {
                             Console.WriteLine(respostes[i, y]);
@@ -234,7 +234,7 @@
                 validAnswer = false;
             }
             Console.WriteLine($"You have finised the test with {encerts} correct answers");
-            MsgNextScreen("Press a key to go to the main menu");
+            MsgNextScreen("Press any key to go to the main menu");
 
         }
         public static void MsgNextScreen(string msg)
