af6e1691858ccb9d4f7a3e863021d282dca338b8
Arnau Cabratosa <a2308739@institutmontilivi.cat>
2024-02-14 20:29:36 +0100

    Hola6

--- quizGithub/Program.cs
+++ quizGithub/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Here is were all is shown
         {
             ConsoleKeyInfo tecla;
             do
